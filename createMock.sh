a5ebc0f9cbddb926ad6ef93cc536cd841441a97f
enrikiko <enrikiko_91@hotmail.com>
2019-06-03 19:47:14 +0200

    auto-commit

--- createMock.sh
+++ createMock.sh
@@ -1,4 +1,4 @@
-ip=$(curl https://5nwdav0wk9.execute-api.eu-central-1.amazonaws.com/dev/get_ip)
+ip=$(curl https://5nwdav0wk9.execute-api.eu-central-1.amazonaws.com/dev/get_ip | tr -d '"')
 sh deviceMock/fpd.sh mock1 8001 $ip
 sh deviceMock/fpd.sh mock4 8004 $ip
 sh deviceMock/fpd.sh mock2 8002 $ip
