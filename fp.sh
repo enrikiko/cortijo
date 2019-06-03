7c75cc85463110c5f4b6c5d432268ca503282b94
enrikiko <enrikiko_91@hotmail.com>
2019-06-03 19:36:35 +0200

    auto-commit

--- fp.sh
+++ fp.sh
@@ -2,7 +2,7 @@ url="http://88.8.67.178:3371"
 device=$1
 echo
 echo "Create New Device"
-curl -X GET $url"/new/"$device"/on/192.2168.1.23:7615"
+curl -X GET $url"/new/"$device"/true/192.2168.1.23:7615"
 echo
 echo
 echo "View Device"
@@ -10,11 +10,11 @@ curl -X GET $url"/name/"$device
 echo
 echo
 echo "Change Device Status"
-curl -X GET $url"/update/"$device"/off"
+curl -X GET $url"/update/"$device"/false"
 echo
 echo
 echo "Create An Existing Device"
-curl -X GET $url"/new/"$device"/on/192.2168.1.23:2000"
+curl -X GET $url"/new/"$device"/true/192.2168.1.23:2000"
 echo
 echo
 echo "Remove Device"
