e8dad333c367bc6a701ab5891e4fd8bb16249b51
enrikiko <enrikiko_91@hotmail.com>
2019-06-04 00:27:39 +0200

    auto-commit

--- fp.sh
+++ fp.sh
@@ -12,17 +12,21 @@ echo
 echo "Change Device Status"
 echo $url"/update/"$device"/false"
 curl -X GET $url"/update/"$device"/false"
+echo
+echo "View Device"
+echo $url"/name/"$device
+curl -X GET $url"/name/"$device
 # echo "Create An Existing Device"
 # curl -X GET $url"/new/"$device"/true/192.2168.1.23:2000"
-echo
-echo
-echo "Remove Device"
-echo $url"/remove/"$device
-curl -X GET $url"/remove/"$device
-echo
-echo
-echo "Remove Device"
-echo $url"/remove/"$device
-curl -X GET $url"/remove/"$device
-echo
-echo
+# echo
+# echo
+# echo "Remove Device"
+# echo $url"/remove/"$device
+# curl -X GET $url"/remove/"$device
+# echo
+# echo
+# echo "Remove Device"
+# echo $url"/remove/"$device
+# curl -X GET $url"/remove/"$device
+# echo
+# echo
--- mfp.sh
+++ mfp.sh
@@ -1,6 +1,6 @@
-name="mock"
 for n in {1..4}
 do
+  name="mock"
   i=$n
   sh fp.sh $name$i
 done
