700bb05bb241e8db7f1d3e265296ec8b2f24a5bb
enrikiko <enrikiko_91@hotmail.com>
2019-06-05 00:53:41 +0200

    auto-commit

--- users.js
+++ users.js
@@ -1,22 +1,16 @@
 const mongoose = require('mongoose');
-// let connString = 'mongodb://mongo:27017/users';
-// const db = mongoose.connection;
-// mongoose.connect(connString);
-//
-// db.on('error',function(){
-// console.log("Error al conectarse a Mongo");
-// });
-//
-// db.once('open', function() {
-// console.log("Conectado a MongoDB");
-// });
-mongoose
-  .connect(
-    'mongodb://mongo:27017/users',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+let connString = 'mongodb://mongo/users';
+const db = mongoose.connection;
+mongoose.connect(connString);
+
+db.on('error',function(){
+console.log("Error al conectarse a Mongo");
+});
+
+db.once('open', function() {
+console.log("Conectado a MongoDB");
+});
+// mongoose.connect('mongodb://192.168.1.50/users',{ useNewUrlParser: true }).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
 
 // definicion de esquema del artículo
 const deviceSchema = new mongoose.Schema({
