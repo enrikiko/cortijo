c56238945be2b76cd34d0751bb4824b4c6068fdc
enrikiko <enrikiko_91@hotmail.com>
2019-06-05 23:16:53 +0200

    auto-commit

--- deviceMock/scs/request.sh
+++ deviceMock/scs/request.sh
@@ -1,6 +1,7 @@
-IP=$local_ip 
+IP=$local_ip
 STATUS="true"
 SERVERIP="$IP:8000"
 echo "$SERVERIP"/new/"$mock_name/$STATUS/$IP:$mock_port"
-RES=$(curl "$SERVERIP"/new/"$mock_name/$STATUS/$IP:$mock_port")
+# RES=$(curl "$SERVERIP"/new/"$mock_name/$STATUS/$IP:$mock_port")
+RES=$(curl 192.168.1.50:8000/new/mock/false/192.168.1.50:8001)
 echo $RES
