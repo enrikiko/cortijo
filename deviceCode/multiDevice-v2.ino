f8bb2465ae345cf4b633e78f9f55f5c7ab5f7b39
Your Name <you@example.com>
2019-06-21 01:01:02 +0200

    auto

---
+++ deviceCode/multiDevice-v2.ino
@@ -0,0 +1,158 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoJson.h>
+
+const char *ssid1 = "Cuarto2.4G";
+const char *password1 = "Lunohas13steps";
+const char *ssid2 = "Cuarto5G";
+const char *password2 = "Lunohas13steps";
+String deviceName = "Device-1";
+int port = 80;
+IPAddress ipDevice(192, 168, 1, 100);
+IPAddress dns(80, 58, 61, 250);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 0, 0, 0);
+String certain;
+
+ESP8266WiFiMulti WiFiMulti;
+ESP8266WebServer server(port);
+
+
+void setup() {
+
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(ssid1, password1);
+  WiFiMulti.addAP(ssid2, password2);
+  WiFi.config(ipDevice, dns, gateway, subnet);
+  WiFi.begin();
+
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  String ip = WiFi.localIP().toString();
+  Serial.println("");
+  Serial.println("");
+  Serial.println("");
+  Serial.print("IP:");
+  Serial.println(ip);
+
+  char pins[9] = {16,5,4,0,2,14,12,13,15};
+  Serial.println(sizeof(pins));
+
+  for (int i = 0; i < sizeof(pins); i++){
+    Serial.println(pins[i]);
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], HIGH);
+    setIp(ip, pins[i]);
+  }
+
+  
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/"+deviceName+"-16/status/true", handleRoot16true);
+  server.on("/"+deviceName+"-16/status/false", handleRoot16false);
+  
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+
+  server.handleClient();
+  //MDNS.update();
+
+}
+
+
+void setIp(String ip, int pin){
+  boolean certain = false;
+  while(!certain){
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+      WiFiClient client;
+      HTTPClient http;
+      Serial.print("[HTTP] begin...\n");
+      if (http.begin(client, "http://192.168.1.50:8000/new/"+deviceName+"-"+pin+"/true/"+ip+":"+port)) {
+        Serial.print("[HTTP] GET CODE: ");
+        int httpCode = http.GET();
+        if (httpCode > 0) {
+          Serial.println(httpCode);
+          if (httpCode == 200 ) {
+            certain = true;
+            Serial.print("[HTTP] GET BODY: ");
+            Serial.println(http.getString());
+          }
+        } else {
+          Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+        http.end();
+      } else {
+        Serial.printf("[HTTP} Unable to connect\n");
+      }
+    }
+    delay(1000);
+  }
+}
+
+void handleInfo() {
+  server.send(200, "application/json", certain);
+}
+
+
+void handleRoot16true() {
+  //certain=true;
+  //light(true);
+  digitalWrite(16, true);
+  Serial.println("pin 16 true");
+  server.send(200, "application/json", "{\"status\": true}");
+}
+void handleRoot16false() {
+  //certain=true;
+  //light(true);
+  digitalWrite(16, false);
+  Serial.println("pin 16 false");
+  server.send(200, "application/json", "{\"status\": false}");
+}
+//void handleStatus() {
+//  StaticJsonDocument<100> doc;
+//  DeserializationError error = deserializeJson(doc, server.arg(0));
+//  setPin(doc);
+//  certain=server.arg(0);
+//  server.send(200, "application/json", server.arg(0));
+//}
+//
+//void setPin(StaticJsonDocument<100> doc){
+//  int pins[9] = {16,5,4,0,2,14,12,13,15};
+//  int tmpVal;
+//  for (int i = 0; i < 9; i++){
+//    tmpVal=doc[String(pins[i])];
+//    digitalWrite(pins[i], !tmpVal);
+//  }
+//}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
