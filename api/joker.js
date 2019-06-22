23fbd04cfac9147dc5d4773572b1964aa013b2f5
enrikiko <enrikiko_91@hotmail.com>
2019-06-22 03:13:18 +0200

    auto-commit

--- api/joker.js
+++ api/joker.js
@@ -33,7 +33,7 @@ module.exports={
 
     log: (text) => {
        //io.emit('chat message', text);
-       text=Date()+"\n"+text+"\n\n"
+       text=',"'+Date()+'"'+":"+'"'+text+'""'
        console.log(text);
        fs.appendFile("log.txt", text, function(err) {
           if(err) {
