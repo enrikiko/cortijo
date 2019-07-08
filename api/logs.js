70e6a09c3f89d43c95729d835de10b8266edefcb
enrikiko <enrikiko_91@hotmail.com>
2019-07-08 23:03:18 +0200

    auto-commit

--- api/logs.js
+++ api/logs.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-let connString = 'mongodb://192.168.1.50:27017/logs';
+let connString = 'mongodb://mongo/logs';
 const db = mongoose.connection;
 mongoose.connect(connString);
 
--- api/users.js
+++ api/users.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-let connString = 'mongodb://192.168.1.50:27017/users';
+let connString = 'mongodb://mongo/users';
 const db = mongoose.connection;
 //mongoose.connect("mongodb://localhost:27017/users");
 mongoose.connect(connString);
