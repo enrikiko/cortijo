70ed40839115ff5feefe83f00994024c697bc319
enrikiko <enrikiko_91@hotmail.com>
2019-07-22 00:11:11 +0200

    Tough somebody west

--- frontAng/src/app/body/body.component.ts
+++ frontAng/src/app/body/body.component.ts
@@ -21,6 +21,15 @@ export class BodyComponent implements OnInit {
 
   getDevicesList(){
     const host = (window.location.href.split("/")[2]).split(":")[0]
+    console.log(host)
+    this.https.get("5nwdav0wk9.execute-api.eu-central-1.amazonaws.com/dev/get_ip").subscribe( data =>
+    {
+      if(data!=null){
+        console.log(data)
+      }
+      else { console.log("No data")
+      }
+    })
     // const host = "88.8.71.214"
     let url = "http://" + host + ":8000/all"
     this.http.get<any[]>(url).subscribe( data =>
