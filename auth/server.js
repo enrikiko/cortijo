0772c1e09e605ccd0e725352f7b6903b6bd463d1
Enrique <enrique.ramos@gemalto.com>
2019-07-23 11:01:39 +0200

    auth

--- auth/server.js
+++ auth/server.js
@@ -76,5 +76,4 @@ app.post('/*', function(req, res){
 // activate the listenner
 http.listen(3000, function () {
     console.log('Servidor activo en http://localhost:3000');
-    console.log(process.env.TOKEN)
   })
