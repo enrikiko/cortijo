1ab1fe23c6e016207de0ceaca57d610230c6ad75
enrikiko <enrikiko_91@hotmail.com>
2019-08-06 19:34:46 +0200

    Sort policy safe yeah

--- frontAng/src/app/app-routing.module.ts
+++ frontAng/src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import { LogsComponent } from './logs/logs.component';
 import { BodyComponent } from './body/body.component';
 import { WeatherComponent } from './weather/weather.component';
 import { AuthGuard } from './auth.guard';
-import { LoginComponent } from './login/login.component'
-import { CatOrDogComponent } from './catordog/catordog.component'
+import { LoginComponent } from './login/login.component';
+import { CatordogComponent } from './catordog/catordog.component'
 
 const routes: Routes = [
   {path:'', component: BodyComponent},
   {path:'logs', component: LogsComponent, canActivate: [AuthGuard]},
   {path:'weather', component: WeatherComponent},
   {path:'weather', component: WeatherComponent},
-  {path:'login', component: LoginComponent}
-  {path:'catordog', component: CatOrDogComponent}
+  {path:'login', component: LoginComponent},
+  {path:'catordog', component: CatordogComponent}
 ];
 
 @NgModule({
