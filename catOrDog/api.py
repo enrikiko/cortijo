b39aea5f3428cf32b596e016e5f046a201469fb8
enrikiko <enrikiko_91@hotmail.com>
2019-08-08 22:50:49 +0200

    Certain summer per land grow remain foot teach

--- catOrDog/api.py
+++ catOrDog/api.py
@@ -14,5 +14,17 @@ def upload_file():
 def liveness():
     return "I am alive"
 
+@app.route('/http', methods = ['GET'])
+def http():
+    return '''<html>
+       <body>
+          <form action = "http://localhost:5000/uploader" method = "POST"
+             enctype = "multipart/form-data">
+             <input type = "file" name = "file" />
+             <input type = "submit"/>
+          </form>
+       </body>
+    </html>'''
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=3000)
