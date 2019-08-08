3b1a3a9849b19b65e60f21af78fa7cdf35d6210e
enrikiko <enrikiko_91@hotmail.com>
2019-08-08 22:33:18 +0200

    Sexual environment that recently anyone baby meeting

--- frontAng/src/app/catordog/catordog.component.ts
+++ frontAng/src/app/catordog/catordog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-catordog',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatordogComponent implements OnInit {
 
-  constructor() { }
+  response: any = "null";
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+  update(event){
+    console.log(event);
+    event.preventDefault();
+    let reader = new FileReader();
+    let formData = new FormData();
+    if(event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      formData.append('upload', file);
+      console.log(file);
+    };
+    const url = "http://88.8.65.164:8030/uploader"
+    const options = {
+      headers: new HttpHeaders().set('Authorization', "EnriqueRamos"),
+    }
+    this.http.post(url, formData, options).subscribe( data =>
+    {
+      if(data!=null){
+        this.response=data;
+        console.log(data);
+      }
+      else {
+      console.log('Database is empty')
+      this.response="No response";
+      }
+    })
+
   }
 
 }
