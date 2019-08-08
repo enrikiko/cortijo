15728dd3f4991da32707e00e678fb0e5c721e62c
enrikiko <enrikiko_91@hotmail.com>
2019-08-08 22:41:10 +0200

    Those home

--- catOrDog/api.py
+++ catOrDog/api.py
@@ -10,5 +10,9 @@ def upload_file():
   f.save(os.path.join(app.instance_path, 'files'), secure_filename(f.filename))
   return 'file uploaded successfully'
 
+@app.route('/liveness', methods = ['GET'])
+def liveness():
+    return "I am alive"
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=3000)
