b42bdbd4acdcbd44e89d0888d492e96d94e3e0fd
Enrique <enrique.ramos@gemalto.com>
2019-08-20 15:09:14 +0200

    link

--- api/ia.js
+++ api/ia.js
@@ -6,7 +6,7 @@ module.exports={
        console.log(req);
        async function getResponse(req) {
          console.log(req)
-         let response = await request.get("http://88.8.65.164/liveness");
+         let response = await request.get("http://88.8.65.164:8200/liveness");
          res = {};
          res.code = response.statusCode;
          res.body = response.body
