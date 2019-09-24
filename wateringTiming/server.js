0dcdf9afa14cae33fcf44911cf7486774eeb9c78
Enrique <enrique.ramos@gemalto.com>
2019-09-24 11:42:12 +0200

    d

--- wateringTiming/server.js
+++ wateringTiming/server.js
@@ -3,10 +3,13 @@ async function call() {
      return await request.get("http://192.168.1.50:8000/update/mock/true ");
 }
 let certain = true
+let time
 while(true){
-     if ( certain && new Date().getMinutes() == 35 && new Date().getHours() == 11){
+     time = new Date()
+     if ( certain && time.getMinutes() == 43 && time.getHours() == 11){
           certain = false
           call()
           console.log("Is time to watering")
-     }else { certain = true}
+     }else if( time.getMinutes() != 35 && time.getHours() != 11){ certain = true}
+     console.log(certain)
 }
