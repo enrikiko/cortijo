ae40a1dcea28ac894e1adb2d13c25b89dc4b743a
enrikiko <enrikiko_91@hotmail.com>
2019-09-26 00:07:04 +0200

    Weight institution

--- functionalPing/functions.sh
+++ functionalPing/functions.sh
@@ -18,9 +18,9 @@ getIp () {
 }
 
 liveness () {
-     CMD="curl --silent -o /dev/null -s -w "%{http_code}\n" $IP:8000/liveness -X GET"
+     CMD="curl --silent -o /dev/null -s -w "%{http_code}" $IP:8000/liveness -X GET"
      VAR=$($CMD)
-     if [ "$VAR" == "200n" ]
+     if [ "$VAR" = "200n" ]
       then
         echo "Liveness works"
       else
