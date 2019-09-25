a0c469f8b793626fe3ecd7f7a99e22bdd395bbc0
enrikiko <enrikiko_91@hotmail.com>
2019-09-25 23:31:00 +0200

    Party another style gas power support high concern

--- functionalPing/functions.sh
+++ functionalPing/functions.sh
@@ -9,12 +9,19 @@ BLUE='\033[0;34m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
+GETIPURL="https://5nwdav0wk9.execute-api.eu-central-1.amazonaws.com/dev/get_ip_clear"
+
 get () {
-     CMD="curl --silent -o /dev/null -s -w "%{http_code}\n" $1 -X GET"
+     CMD="curl --silent -o /dev/null -s -w "%{http_code}\n" $IP:8000/liveness -X GET"
      # $CMD > /tmp/out.txt
-     VAR= cat /tmp/out.txt
+     VAR= $CMD
 }
 result () {
      # cat /tmp/out.txt
      echo $VAR
+}
+
+getIp () {
+     CMD="curl --silent $GETIPURL -X GET"
+     IP= $CMD
 }
--- functionalPing/ping.sh
+++ functionalPing/ping.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . ./functions.sh
-get "https://5nwdav0wk9.execute-api.eu-central-1.amazonaws.com/dev/get_ip_clear"
+get 
 result
