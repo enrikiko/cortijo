dd52287313fbf3aedac752208f032519d7b5c905
enrikiko <enrikiko_91@hotmail.com>
2019-09-27 18:50:26 +0200

    Reality response

--- wateringTiming/server.js
+++ wateringTiming/server.js
@@ -2,18 +2,17 @@ const request = require('superagent');
 async function call() {
      console.log("calling...")
      return await request.get("http://192.168.1.50:8000/update/mock/true");
-     console.log("calling")
 }
 let certain = true
 let time
-const min = 23
-const hour = 17
+const min = 52
+const hour = 18
 while(true){
      time = new Date()
      if ( certain & time.getMinutes() == min & time.getHours() == hour){
           certain = false
           call()
-          // console.log(res)
+          console.log(time)
           console.log("Is time to watering")
      }else if( !certain & (time.getMinutes() != min | time.getHours() != hour)){
           certain = true
