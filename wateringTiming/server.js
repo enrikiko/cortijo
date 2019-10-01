dd6b61c20a23f26a34bde14ca6985871b5cffd22
enrikiko <enrikiko_91@hotmail.com>
2019-10-01 23:40:29 +0200

    Model win leader long cold fear

--- wateringTiming/server.js
+++ wateringTiming/server.js
@@ -1,21 +1,47 @@
 const request = require('superagent');
-async function call() {
+async function on() {
+  http.get({
+    hostname: '192.168.1.50',
+    port: 8000,
+    path: '/update/mock/true',
+    agent: false  // Create a new agent just for this one request
+  }, (res) => {
+    // Do stuff with response
+    console.log(res)
+  });
+}
+async function off() {
      console.log("calling...")
-     let res = await request.get("http://192.168.1.50:8000/update/mock/true");
+     let res = await request.get("http://192.168.1.50:8000/update/mock/false");
+     return res
+}
+async function start() {
+     console.log("starting...")
+     let res = await on();
+     console.log(res)
+     return res
+}
+async function stop() {
+     console.log("stopping...")
+     let res = await off();
+     console.log(res)
      return res
 }
 let certain = true
 let time
-const min = 0
-const hour = 19
+const min = 9
+const hour = 9
 while(true){
      time = new Date()
      if ( certain & time.getMinutes() == min & time.getHours() == hour){
           certain = false
           console.log(time)
           console.log("Is time to watering")
-          console.log(call())
+          on()
      }else if( !certain & (time.getMinutes() != min | time.getHours() != hour)){
           certain = true
+          console.log(time)
+          console.log("Is time to stop watering")
+          stop()
      }
 }
---
+++ wateringTiming/server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+time="$(date +"%H:%M")"
+waterTime="23:59"
+certain="true"
+
+function water
+{
+  if [[ $certain = "true" ]]; then
+    realTime="$(date)"
+    echo $realTime
+    echo "Is time to watter"
+    certain="false"
+    curl  http://192.168.1.50:8000/update/mock/true
+  fi
+
+}
+
+function no-water
+{
+  if [[ $certain = "false" ]]; then
+    echo "Is not time to watter"
+    certain="true"
+  fi
+}
+
+function check-time {
+  if [ $1 == $2 ]
+  then
+    water
+  else
+    no-water
+  fi
+}
+
+while [[ 1 ]]; do
+  time="$(date +"%H:%M")"
+  check-time $time $waterTime
+  sleep 4
+done
