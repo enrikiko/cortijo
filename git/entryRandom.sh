94385f433e4606cd2c3770c28ee97f8b797d9963
enrikiko <enrikiko_91@hotmail.com>
2019-10-08 21:53:25 +0200

    Dinner skill without religious anything realize mrs

---
+++ git/entryRandom.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+echo "Starting entry.sh"
+git config --global user.email "enrikiko_91@hotmail.com"
+git config --global user.name "enrikiko_91"
+cd ./Java
+git pull
+cd ../
+date
+date
+while [ 1 ]
+do
+     date
+     repetition=$((1 + RANDOM % 10))
+     delay=$((1 + RANDOM % 23))
+     echo $repetition
+     echo delay
+     then
+          for i in {1..$repetition}
+          do
+               sh commit.sh
+          done
+     fi
+     sleep "$delay"h
+done
