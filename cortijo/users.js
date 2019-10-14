034264d62a5b40a3a78bdca6176db33568ccee82
enrikiko <enrikiko_91@hotmail.com>
2019-10-14 22:34:01 +0200

    None during appear song from share cover

--- cortijo/users.js
+++ cortijo/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const logs = require('./logs');
-let connString = 'mongodb://user_name:user_password@192.168.1.50:27017/users';
+let connString = 'mongodb://192.168.1.50:27017/users';
 const db = mongoose.connection;
 //mongoose.connect("mongodb://user_name:password@172.18.0.5:27017/users");
 mongoose.connect(connString);
