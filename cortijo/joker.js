3ea4414c15007060c7686bd8792f5e6364b1bce3
enrikiko <enrikiko_91@hotmail.com>
2019-10-19 01:27:52 +0200

    Moment each capital over

--- cortijo/joker.js
+++ cortijo/joker.js
@@ -32,7 +32,7 @@ module.exports={
        return await getResponse(url);
      },
      verifyJwt: async (jwt) => {
-       const url = "http://192.168.1.50:8010/auth/jwt/"jwt
+       const url = "http://192.168.1.50:8010/auth/jwt/"+jwt
        async function getResponse(url) {
          let response = await request.get(url);
          return response.jwt;
