8423fbcef333b26435b75be16e956ee12f80edf5
enrikiko <enrikiko_91@hotmail.com>
2019-10-23 23:49:29 +0200

    Respond hotel list people talk through

--- frontAngTest/src/app/photos/photos.component.ts
+++ frontAngTest/src/app/photos/photos.component.ts
@@ -20,7 +20,7 @@ export class PhotosComponent implements OnInit {
 
   getphoto(folder){
     const host = (window.location.href.split("/")[2]).split(":")[0]
-    let url = "http://" + host + ":8400/" + date + "/" folder
+    let url = "http://" + host + ":8400/" + date + "/" + folder
     this.http.get<any[]>(url).subscribe( data =>
     {
       if(data!=null){
