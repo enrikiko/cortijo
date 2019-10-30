e4db75b1b1dba30cdf3a1f5e3cf274a992371a5d
Enrique <enrique.ramos@gemalto.com>
2019-10-30 08:37:45 +0100

    change time

--- wateringTiming/server.sh
+++ wateringTiming/server.sh
@@ -4,7 +4,7 @@ echo "start script server.sh"
 #time="$(date +"%H:%M")"
 waterTime="17:30"
 certain="true"
-wateringTime=300000 #5min
+wateringTime=180000 #3min
 
 function on
 {
@@ -13,14 +13,15 @@ function on
     echo $realTime
     echo "Is time to watter"
     certain="false"
-    curl  http://192.168.1.50:8000/update/Watering/true/$wateringTime
+    curl --silent http://192.168.1.50:8000/update/Watering/true/$wateringTime
+    echo
   fi
 }
 
 function off
 {
   if [[ $certain = "false" ]]; then
-    echo "Is not time to watter"
+    #echo "Is not time to watter"
     certain="true"
   fi
 }
