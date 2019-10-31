ec29d1d2fdddfe7b33c39e1a345cf206e6fc4ef8
enrikiko <enrikiko_91@hotmail.com>
2019-11-01 00:32:54 +0100

    Population activity group himself seven stock i maybe

--- frontAngTest/src/app/logs/logs.component.ts
+++ frontAngTest/src/app/logs/logs.component.ts
@@ -19,7 +19,7 @@ export class LogsComponent implements OnInit {
   getLogs(){
     const host = (window.location.href.split("/")[2]).split(":")[0]
     // const host = "88.8.65.164"
-    let url = "http://" + host + ":8000/log/history"
+    let url = "http://" + host + ":8000/all/log"
     //this.http.get<HttpResponse<object>>(url).subscribe( data =>
     this.http.get<any[]>(url).subscribe( data =>
     {
