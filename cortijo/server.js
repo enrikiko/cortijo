8fbecbd40532636cffa021153f33d6b694044f21
enrikiko <enrikiko_91@hotmail.com>
2019-11-19 00:50:35 +0100

    Bar effort

--- cortijo/server.js
+++ cortijo/server.js
@@ -229,7 +229,6 @@ app.get("/status/:device", async function(req, res) {
   }else {
     try {
       var status = await joker.getDeviceStatus(ip, name) //Get device status
-      console.log("status: " + status)
       res.status(200).json(status)
     }catch (e) {
       console.log("no response")
--- functionalPing/check.py
+++ functionalPing/check.py
@@ -5,10 +5,13 @@ response = requests.get(url=URL)
 response = response.json()
 for device in response:
     devices.append(device['name'])
-    URL = "http://88.8.38.4:8000/status/" + str(device['name'])
+    URL = "http://88.8.38.4:8000/status/" + device['name']
     res = requests.get(url=URL)
-    print(res)
-    res = res.json
-    print(res)
-    print(res)
+    if res.status_code == 200:
+        res = res.json()
+        print(res["status"])
+    elif res.status_code == 404:
+        URL = "http://88.8.38.4:8000/remove/" + device['name']
+        res = requests.get(url=URL)
+        print(res.json())
 print(devices)
