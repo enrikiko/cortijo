2409457e3014433c7433bcc7eea638e3c287f082
enrikiko <enrikiko_91@hotmail.com>
2019-12-04 23:16:50 +0100

    Other story

--- cortijo/joker.js
+++ cortijo/joker.js
@@ -26,7 +26,7 @@ module.exports={
        return await getResponse();
      },
 
-     switchAlertLapse: ( name, lapse ) => {
+     switchAlertLapse: async ( name, lapse ) => {
         ip = myDevice.getIdByName(name)
         text = name+" has changed to true during "+lapse+" miliseconds"
         const url = "https://us-central1-afrodita-2e204.cloudfunctions.net/triggerPushNotification?token=dPM2s9vYj4o:APA91bG3LiZsdvj7EPqBlTHKNXCiDbpWDdxKhONAO_qpIf_8uomgVW5QFtxM2AIX0kJPPt3RBzPJVeMNMgkCTtfkUoJFAHYtPBROh6bupxDkxW647z7J4A8Y3690q7OV6_lkYIvt7dlA&title=" + text
@@ -34,7 +34,7 @@ module.exports={
         watering.newRequest(name, lapse, true)
      },
 
-     switchAlert: ( name ) => {
+     switchAlert: async ( name ) => {
         ip = myDevice.getIdByName(name)
         text = name+" has changed to false"
         const url = "https://us-central1-afrodita-2e204.cloudfunctions.net/triggerPushNotification?token=dPM2s9vYj4o:APA91bG3LiZsdvj7EPqBlTHKNXCiDbpWDdxKhONAO_qpIf_8uomgVW5QFtxM2AIX0kJPPt3RBzPJVeMNMgkCTtfkUoJFAHYtPBROh6bupxDkxW647z7J4A8Y3690q7OV6_lkYIvt7dlA&title=" + text
