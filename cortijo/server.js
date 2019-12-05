b24afaab6f62e829a3503984cca8417cd25bb3de
enrikiko <enrikiko_91@hotmail.com>
2019-12-06 00:58:41 +0100

    Field keep while store financial save choose enough

--- cortijo/server.js
+++ cortijo/server.js
@@ -17,7 +17,6 @@ const auth = require('basic-auth');
 const app = express();
 const fs = require('fs')
 const cookieParser = require('cookie-parser');
-const parseBearerToken = require('parse-bearer-token');
 app.enable('trust proxy');
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -110,11 +109,9 @@ app.get("/new/:name/:status/:ip", async (req, res) => {
 
  //JWT verification
  app.get("/jwt", async function(req, res) {
-   const token = parseBearerToken(req)
-   console.log(token)
    const jwt = req.headers.authorization
    console.log(jwt)
-   res.status(200).json({"jwt":jwt, "token":token})
+   res.status(200).json({"jwt":jwt})
 //   if( requireJwt==false || jwt!=undefined ){next()}
 //   else{
 //     console.log("jwt is undefined")
