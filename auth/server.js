838324dee61d0b03c25f4e6896026ff1242afac7
enrikiko <enrikiko_91@hotmail.com>
2019-12-06 02:18:48 +0100

    Another join election

--- auth/server.js
+++ auth/server.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
 const app = express();
 app.enable('trust proxy');
-app.use(express.bodyParser());
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 app.options('*', cors());
