b983e37fadb961fc7288d348e90c015e0c9a7444
enrikiko <enrikiko_91@hotmail.com>
2019-12-10 21:59:55 +0100

    Product speech available study concern more inside why

--- frontAngTest/src/app/header/header.component.ts
+++ frontAngTest/src/app/header/header.component.ts
@@ -14,7 +14,7 @@ export class HeaderComponent implements OnInit {
 //  temperature: string="";
 //  humidity: string="";
   data : any[]=null;
-  jwt : string="null";
+  jwt : string=null;
 
   constructor(private Http: HttpClient,
               private Auth: AuthService) { }
