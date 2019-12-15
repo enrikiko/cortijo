f0e0fcb55b9ffd22fddb4b6b24dba35fbb9a1cd3
enrikiko <enrikiko_91@hotmail.com>
2019-12-15 15:55:50 +0100

    Debate area ago nothing wonder

--- sensorMock/scs/request.sh
+++ sensorMock/scs/request.sh
@@ -1,7 +1,7 @@
 IP=$local_ip
 STATUS="false"
 SERVERIP="$IP:8000"
-echo "$SERVERIP"/new/"$mock_name/$STATUS/$IP:$mock_port"
+echo "$SERVERIP"/newSensor/"$mock_name/$STATUS/$IP:$mock_port"
 RES=$(curl $SERVERIP"/newSensor/"$mock_name/$STATUS/$IP:$mock_port)
 sleep 1000
 RES=$(curl $SERVERIP"/newSensor/"$mock_name/$STATUS/$IP:$mock_port)
