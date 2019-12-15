a66f2636b46a736d614faf4c905118c87d6cb60d
enrikiko <enrikiko_91@hotmail.com>
2019-12-15 16:33:30 +0100

    Play doctor test

--- cortijo/timeout.js
+++ cortijo/timeout.js
@@ -1,4 +1,5 @@
 const sensors = require('./sensors');
+const myDevice = require('./devices');
 const logs = require('./logs');
 const request = require('superagent');
 const req = require('request');
@@ -7,18 +8,26 @@ const yaml = require('js-yaml')
 // Get config
 const config_file = fs.readFileSync('config.yaml');
 const config = yaml.safeLoad(config_file);
-const TIMEOUT = config.timeout
+const TIMEOUT_SENSOR = config.timeout_sensor
+const TIMEOUT_CHECK = config.timeout_check
 //
-function executeTimeout(){
+function executeTimeoutSensor(){
     setTimeout(function(){
-            executeTimeout()
-            logs.log("Time out executing every " + TIMEOUT + " milliseconds")
+            executeTimeoutSensor()
+            logs.log("Time out sensor executing every " + TIMEOUT_SENSOR + " milliseconds")
             getSensor()
-        }, TIMEOUT);
+        }, TIMEOUT_SENSOR);
+}
+function executeTimeoutCheck(){
+    setTimeout(function(){
+            executeTimeoutCheck()
+            logs.log("Time out check executing every " + TIMEOUT_CHECK + " milliseconds")
+            check()
+        }, TIMEOUT_CHECK);
 }
 //
 async function getSensor(){
-    logs.log("getSensor fun")
+    logs.log("getSensor...")
     var sensorList = await sensors.getAllSensor()
     logs.log(sensorList)
     for (var sensor in sensorList){
@@ -26,4 +35,15 @@ async function getSensor(){
     }
 }
 //
-executeTimeout()
+async function check(){
+    logs.log("check...")
+    var devicesList = await myDevice.getDevice()
+    logs.log(devicesList)
+    for (var device in devicesList){
+        logs.log(device.name)
+        await myDevice.checkDeviceByName(device.name)
+    }
+}
+//
+executeTimeoutSensor()
+executeTimeoutCheck()
