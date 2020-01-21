718d4756317527a9bac167823cd8764960b1a6b1
enrikiko <enrikiko_91@hotmail.com>
2020-01-21 23:20:53 +0100

    War travel

--- cortijo/humidity.js
+++ cortijo/humidity.js
@@ -46,5 +46,5 @@ module.exports = {
     });
   },
 
-  getAll: async(name) => { return await myHumidity.find({"name":name}).limit(2)}
+  getAll: async(name) => { return await myHumidity.find({"name":name}).sort({time:-1}).limit(2)}
 }
