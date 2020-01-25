b8a71dc917ca90ff82614a0feb2bef29e2ff84ed
enrikiko <enrikiko_91@hotmail.com>
2020-01-25 17:29:47 +0100

    Fine fire former establish discover step

--- frontAngTest/src/app/auth.service.ts
+++ frontAngTest/src/app/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
     })
     const host = (window.location.href.split("/")[2]).split(":")[0]
     const url = "http://" + host + ":8000/jwt"
-    this.http.get(url, { headers: headers }).subscribe( data => {
+    this.http.get<any>(url, { headers: headers }).subscribe( data => {
       if(data!=null){
         console.log(data)
       }
