6cdbd478fc0a5c1bfab570409bf6c2ba4c59e9e2
enrikiko <enrikiko_91@hotmail.com>
2020-01-26 22:36:44 +0100

    Wall tell own especially grow

--- webSocker/newserver.js
+++ webSocker/newserver.js
@@ -22,10 +22,11 @@ app.ws('/', function(ws, req) {
   save(ws)
   ws.on('message', function(msg) {
     console.log(msg);
-    //ws.send(msg);
-    wsList.forEach(function each(client) {
-      if (client.readyState==1) {
-        client.send(msg);
+    wsList.forEach(function each(ws) {
+      if (ws.readyState==1) {
+        ws.send(msg);
+      }else{
+        deleteWS(ws)
       }
     });
   });
@@ -34,19 +35,25 @@ app.ws('/', function(ws, req) {
 
 function save(ws) {
   console.log("save");
-  console.log("is"+wsList.includes(ws))
   if(!wsList.includes(ws)){
     wsList.push(ws)
   }
-  console.log(wsList);
+  printList(wsList)
 }
 
 function deleteWS(ws) {
+  console.log("save");
   const index = wsList.indexOf(ws);
   if (index > -1) {
     wsList.splice(index, 1);
   }
-  console.log(wsList);
+  printList(wsList)
+}
+
+function printList(wsList) {
+  wsList.forEach((item, i) => {
+    console.log((item.readyState, i));
+  });
 }
 
 app.listen(PORT);
