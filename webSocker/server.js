f22e315e5594c4058f4fec4cead9524abc3eba9d
enrikiko <enrikiko_91@hotmail.com>
2020-01-26 17:57:17 +0100

    Five respond somebody choose cover heat town

--- webSocker/server.js
+++ webSocker/server.js
@@ -2,18 +2,18 @@ var WebSocketServer = require('ws').Server
 var wss = new WebSocketServer({ port: 3000 })
 
 wss.on('connection', function(ws) {
-  console.log('connection2';
+  console.log('connection2')
   wss.clients.forEach(function(client) {
     if (client.readyState == 1) {
       client.send("New connection", Date.now())
-      console.log('New Connection', Date.now());
+      console.log('New Connection', Date.now())
     }
   });
 
   wss.on('connection', function(ws) {
-    console.log('connection2';
+    console.log('connection2')
     ws.on('message', function(data) {
-      console.log('message2';
+      console.log('message2')
       wss.clients.forEach(function each(client) {
         if (client.readyState==1) {
           client.send(data);
