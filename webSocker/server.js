a1b8f89461c09d13344472a761b4d7702b8d53c6
enrikiko <enrikiko_91@hotmail.com>
2020-01-26 17:43:04 +0100

    Violence she billion within thought letter door

--- webSocker/server.js
+++ webSocker/server.js
@@ -2,8 +2,14 @@ var WebSocketServer = require('ws').Server
 var wss = new WebSocketServer({ port: 3000 })
 
 wss.on('connection', function(ws) {
-  ws.send(Date.now())
-  console.log('New Connection', Date.now());
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocketServer.OPEN) {
+      
+      client.send(Date.now())
+      console.log('New Connection', Date.now());
+    }
+  });
+
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocketServer.OPEN) {
