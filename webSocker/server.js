32be3677780692f721a764bf3c20b4057867add3
enrikiko <enrikiko_91@hotmail.com>
2020-01-26 17:59:32 +0100

    Oh method maintain stand operation fly article

--- webSocker/server.js
+++ webSocker/server.js
@@ -2,23 +2,19 @@ var WebSocketServer = require('ws').Server
 var wss = new WebSocketServer({ port: 3000 })
 
 wss.on('connection', function(ws) {
-  console.log('connection2')
+  console.log('connection1')
   wss.clients.forEach(function(client) {
     if (client.readyState == 1) {
       client.send("New connection", Date.now())
       console.log('New Connection', Date.now())
     }
   });
-
-  wss.on('connection', function(ws) {
-    console.log('connection2')
-    ws.on('message', function(data) {
-      console.log('message2')
-      wss.clients.forEach(function each(client) {
-        if (client.readyState==1) {
-          client.send(data);
-        }
-      });
+  ws.on('message', function(data) {
+    console.log('message2')
+    wss.clients.forEach(function each(client) {
+      if (client.readyState==1) {
+        client.send(data);
+      }
     });
   });
 })
