4ab3db12ede1a38c0060f83bf127da791a7ecb76
enrikiko <enrikiko_91@hotmail.com>
2020-01-26 17:23:53 +0100

    Any admit recent study person management

--- webSocker/server.js
+++ webSocker/server.js
@@ -2,6 +2,7 @@ var WebSocketServer = require('ws').Server
 var wss = new WebSocketServer({ port: 3000 })
 
 wss.on('connection', function(ws) {
+  ws.send(Date.now())
   console.log('New Connection', Date.now());
 })
 
--- wstest/init.js
+++ wstest/init.js
@@ -9,16 +9,26 @@ ws.onclose = function() {
 }
 
 ws.onmessage = function(payload) {
-  printMessege(payload.data)
+  console.log("payload")
+  console.log(payload)
 }
 
-// Connection opened
 ws.addEventListener('open', function (event) {
     ws.send('Hello Server!');
 });
 
 // Listen for messages
 ws.addEventListener('message', function (event) {
-  alert()
-  //  console.log('Message from server ', event.data);
+  //alert()
+  console.log('Message from server ', event.data);
 });
+
+function test() {
+  console.log("test")
+  ws.send("Hi")
+}
+
+function setTitle(title) {
+  document.querySelector('h1').innerHTML = title
+  console.log('New connection');
+}
