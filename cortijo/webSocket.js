d974f032750f4cfcec633d8b424a70905c58551d
enrikiko <enrikiko_91@hotmail.com>
2020-01-31 18:38:39 +0100

    Sister fish write live threat congress boy style

--- cortijo/webSocket.js
+++ cortijo/webSocket.js
@@ -1,26 +1,26 @@
 const WebSocket = require('ws');
-let socket = new WebSocket("wss://88.7.67.229:8200");
+let socket = new WebSocket("wss://localhost:8200");
 
 socket.onopen = function(e) {
-cosole.log("[open] Connection established");
-cosole.log("Sending to server");
+console.log("[open] Connection established");
+console.log("Sending to server");
   socket.send("My name is John");
 };
 
 socket.onmessage = function(event) {
-cosole.log(`[message] Data received from server: ${event.data}`);
+console.log(`[message] Data received from server: ${event.data}`);
 };
 
 socket.onclose = function(event) {
   if (event.wasClean) {
-    cosole.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
   } else {
     // e.g. server process killed or network down
     // event.code is usually 1006 in this case
-    cosole.log('[close] Connection died');
+    console.log('[close] Connection died');
   }
 };
 
 socket.onerror = function(error) {
-cosole.log(`[error] ${error.message}`);
+console.log(`[error] ${error.message}`);
 };
