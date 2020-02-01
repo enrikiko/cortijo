b537635fb873cdb2f23266bb06f2b04408ff8bde
enrikiko <enrikiko_91@hotmail.com>
2020-02-02 00:26:35 +0100

    Town send recently stop

--- cortijo/webSocket.js
+++ cortijo/webSocket.js
@@ -2,27 +2,27 @@ const WebSocket = require('ws');
 var ws = new WebSocket('ws://88.7.67.229:8200','echo-protocol')
 
 ws.onopen = function(e) {
-console.log("[open] Connection established");
+console.log("[WebSocket:open] Connection established");
 console.log("Sending to server");
   ws.send("Hello my name is Cortijo");
 };
 
 ws.onmessage = function(event) {
-console.log(`[message] Data received from server: ${event.data}`);
+console.log(`[WebSocket:message] Data received from server: ${event.data}`);
 };
 
 ws.onclose = function(event) {
   if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    console.log(`[WebSocket:close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
   } else {
     // e.g. server process killed or network down
     // event.code is usually 1006 in this case
-    console.log('[close] Connection died');
+    console.log('[WebSocket:close] Connection died');
   }
 };
 
 ws.onerror = function(error) {
-console.log(`[error] ${error.message}`);
+console.log(`[WebSocket:error] ${error.message}`);
 };
 
 module.exports = {
