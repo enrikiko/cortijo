bc019e8715746fc4a4f6c0051118666c9d709bca
enrikiko <enrikiko_91@hotmail.com>
2020-02-02 20:16:37 +0100

    According hotel finish hot

--- webSocket/miguel.js
+++ webSocket/miguel.js
@@ -1,31 +1,22 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
-
-// initializing server
 const app = express();
-const serveStatic = require("serve-static")
 const server= http.createServer(app);
 const io = socketio.listen(server);
 const path = require('path');
 const cors = require('cors');
 const port = 3000;
-
 app.use(cors());
 
 
-// settings
-app.set('port', process.env.PORT || 3001);
+const server = require('http').createServer();
 
-// sockets
-require('./sockets')(io);
+const io = require('socket.io')(server);
 
-// starting the server
-// server.listen(app.get('port'), () => {
-//   console.log(`server on port ${app.get('port')}`);
-// });
-
-
-server.listen(port, () => {
-  console.log("Server on port " + port);
+io.on('connection', client => {
+  client.on('event', data => { /* … */ });
+  client.on('disconnect', () => { /* … */ });
 });
+
+server.listen(3000);
