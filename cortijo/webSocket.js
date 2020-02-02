1f1d853945f82c1c12b192eec08433b9fcf5b9f9
enrikiko <enrikiko_91@hotmail.com>
2020-02-02 12:35:47 +0100

    Seek reveal

--- cortijo/webSocket.js
+++ cortijo/webSocket.js
@@ -1,4 +1,5 @@
 const WebSocket = require('ws');
+const delay = require('delay');
 var ws = new WebSocket('ws://88.7.67.229:8200','echo-protocol')
 
 ws.onopen = function(e) {
@@ -19,12 +20,28 @@ ws.onclose = function(event) {
     // event.code is usually 1006 in this case
     console.log('[WebSocket:close] Connection died');
   }
-  ws = new WebSocket('ws://88.7.67.229:8200','echo-protocol')
+  reconnect()
 };
 
 ws.onerror = function(error) {
   console.log(`[WebSocket:error] ${error.message}`);
 };
+
+function reconnect() {
+  retry=true
+  while (retry) {
+    try {
+      ws = new WebSocket('ws://88.7.67.229:8200','echo-protocol')
+    } catch (e) {
+      console.log(e);
+    } finally {
+      retry=false
+      console.log("Reconnect to ws://88.7.67.229:8200");
+    }
+    await delay(1000)
+  }
+
+}
 
 module.exports = {
   send:(msg)=>{ws.send(msg)}
