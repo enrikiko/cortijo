90db712243fb9d26cea5088e8bcd26bb0f9f3ab6
enrikiko <enrikiko_91@hotmail.com>
2020-02-02 22:13:29 +0100

    Few along spend ask school ready choice some

--- webSocket/miguel.js
+++ webSocket/miguel.js
@@ -1,14 +1,3 @@
-const express = require('express');
-var cors = require('cors')
-const app = express();
-app.use(cors());
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-io.on('connection', client => {
-  console.log("Something happen");
-  client.on('event', data => { /* … */ });
-  client.on('disconnect', () => { /* … */ });
-});
-
-server.listen(3000);
+const io = require('socket.io')();
+io.on('connection', client => { console.log(client); });
+io.listen(3000);
