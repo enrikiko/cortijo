dcd9f7328f77a959d27477431611e68d4bc974b7
enrikiko <enrikiko_91@hotmail.com>
2020-02-02 20:14:19 +0100

    Hang i effort majority

--- cortijo/webSocket.js
+++ cortijo/webSocket.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 const delay = require('delay');
-var ws = new WebSocket('ws://88.7.67.229:8200','echo-protocol')
+//var ws = new WebSocket('ws://88.7.67.229:8200','echo-protocol')
 var ws = new WebSocket('ws://api.darwinex.com/quotewebsocket/1.0.0','echo-protocol')
 
 
