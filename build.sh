f4e40ab2e98748c3322c48164391be89ebf3e2b2
Enrique <enrique.ramos@gemalto.com>
2020-02-07 14:18:42 +0100

    change build files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 #path=/root 
-cd ~/deployment/cortijo
+cd ~/cortijo/deployment
 git pull
 docker-compose build --build-arg MONGO_USER=user_name --build-arg MONGO_PASSWORD=user_password --build-arg PASSWORD=xxxxxx
 docker-compose up -d --remove-orphans
