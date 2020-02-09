3dbb0cd5ef3f867863188a9c77079d19bd76cb37
enrikiko <enrikiko_91@hotmail.com>
2020-02-09 12:02:15 +0100

    Voice activity order visit shot line

--- sensorHumidityMock/scs/request.sh
+++ sensorHumidityMock/scs/request.sh
@@ -1,8 +1,8 @@
 IP=$local_ip
 STATUS="false"
 SERVERIP="$IP:8000"
-echo "$SERVERIP"/newSensor/humidity/"$mock_name/$IP:$mock_port"
-RES=$(curl $SERVERIP"/newSensor/humidity/"$mock_name/$IP:$mock_port)
+echo "$SERVERIP"/newSensor/humidity/"$mock_name/$IP:$mock_port"?devices=mock,mock2
+RES=$(curl $SERVERIP"/newSensor/humidity/"$mock_name/$IP:$mock_port"?devices=mock,mock2")
 sleep 1000
-RES=$(curl $SERVERIP"/newSensor/humidity/"$mock_name/$IP:$mock_port)
+RES=$(curl $SERVERIP"/newSensor/humidity/"$mock_name/$IP:$mock_port"?devices=mock,mock2")
 echo $RES
--- sensorMock/scs/request.sh
+++ sensorMock/scs/request.sh
@@ -1,8 +1,9 @@
 IP=$local_ip
 STATUS="false"
 SERVERIP="$IP:8000"
-echo "$SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=['mock','mock2']
-RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=['mock','mock2']")
+echo "$SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock,mock2
+RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock,mock2")
+echo $RES
 sleep 1000
-RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=['mock','mock2']")
+RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock,mock2")
 echo $RES
