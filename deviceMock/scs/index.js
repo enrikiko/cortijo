ccf8992f5b06a9961ca386ed470d2bff86e697a1
enrikiko <enrikiko_91@hotmail.com>
2020-02-09 23:39:38 +0100

    Military surface skill officer

--- deviceMock/scs/index.js
+++ deviceMock/scs/index.js
@@ -33,7 +33,7 @@ app.get('/'+process.env.mock_name+'/status/true', async function(req, res){
     console.log(info);
     status=true;
     try{
-      let value = await request.get("http://192.168.1.50:8105/test")
+      let value = await request.get("http://192.168.1.50:8106/test")
     }catch (e) {
       console.log(e);
     }
