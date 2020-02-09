cd177aba427ef0a87340c6a244da585a8ec34541
enrikiko <enrikiko_91@hotmail.com>
2020-02-09 21:25:34 +0100

    Also involve yeah

--- cortijo/sensors.js
+++ cortijo/sensors.js
@@ -224,7 +224,7 @@ module.exports = {
       }
       else if (list.length > 0) {
         var sensor = list[0]
-        var devices = sensor.devices
+        var devices = sensor.devices["0"].splice(",")
         logs.log("type of device:" + typeof(devices))
         return devices
       }else {
--- cortijo/timeout.js
+++ cortijo/timeout.js
@@ -86,11 +86,11 @@ async function analiceData(type,name,data) {
     var min = await mySensor.getMin(name)
     var max = await mySensor.getMax(name)
     var devices = await mySensor.getDevices(name)
-    logs.log(typeof(devices["0"]))
-    logs.log(devices["0"])
-    for (const property in devices) {
-      console.log(`${property}: ${devices[property]}`);
-    }
+    //logs.log(typeof(devices["0"]))
+    //logs.log(devices["0"])
+    //for (const property in devices) {
+    //  console.log(`${property}: ${devices[property]}`);
+    //}
     //logs.log("Name: " + name + " Type: " + type + " Content: " + data.humidity + " Min:" + min + " Max: " + max + " Devices " + devices)
     if (min!=undefined&max!=undefined&devices.length>0) {
       logs.log("Name: " + name + " Type: " + type + " Content: " + data.humidity + " Min:" + min + " Max: " + max + " Devices " + devices)
