1baa248b1cdaa19feab7eca83b1d5e4f6c633c15
enrikiko <enrikiko_91@hotmail.com>
2020-02-12 22:49:51 +0100

    Finger put man simple young

--- cortijo/webSocket.js
+++ cortijo/webSocket.js
@@ -1,29 +1,30 @@
 const WebSocket = require('ws');
 const delay = require('delay');
+const logs = require('./logs');
 var ws = new WebSocket('ws://192.168.1.50:8200','echo-protocol')
 //var ws = new WebSocket('https://protected-crag-00706.herokuapp.com/','echo-protocol')
 
 
 ws.onopen = function(e) {
-  console.log("[WebSocket:open] Connection established");
-  console.log("[WebSocket:messege] Sending to server");
+  logs.log("[WebSocket:open] Connection established");
+  logs.log("[WebSocket:messege] Sending to server");
   ws.send(" { 'op': 'subscribe', 'productNames' : [ DWC.4.20 ] }");
 };
 
 ws.onmessage = function(event) {
-  console.log(`[WebSocket:message] Data received from server: ${event.data}`);
+  logs.log(`[WebSocket:message] Data received from server: ${event.data}`);
 };
 
 ws.onclose = function(event) {
   if (event.wasClean) {
-    console.log(`[WebSocket:close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    logs.log(`[WebSocket:close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
   } else {
-    console.log('[WebSocket:close] Connection died');
+    logs.log('[WebSocket:close] Connection died');
   }
 };
 
 ws.onerror = function(error) {
-  console.log(`[WebSocket:error] ${error.message}`);
+  logs.log(`[WebSocket:error] ${error.message}`);
 };
 
 
