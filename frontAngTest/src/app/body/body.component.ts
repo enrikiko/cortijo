b02b8677d32883099432ba9d37562d569c3c15e7
enrikiko <enrikiko_91@hotmail.com>
2020-02-29 21:04:10 +0100

    Alone reduce resource

--- frontAngTest/src/app/body/body.component.ts
+++ frontAngTest/src/app/body/body.component.ts
@@ -14,6 +14,7 @@ import { Event } from '../event';
 export class BodyComponent implements OnInit {
 
   devices: any[] = null;
+  sensors: any[] = null;
   lapse: number = null;
   lapse_time: number = 5;
   messages: Message[] = [];
