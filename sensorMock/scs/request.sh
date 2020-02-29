32c78493a10fab1ae64683dd3ff8441d5084363a
enrikiko <enrikiko_91@hotmail.com>
2020-03-01 00:53:44 +0100

    Enough health kill begin rate surface you

--- sensorMock/scs/request.sh
+++ sensorMock/scs/request.sh
@@ -2,8 +2,8 @@ IP=$local_ip
 STATUS="false"
 SERVERIP="$IP:8000"
 echo "$SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock,mock2
-RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock,mock2")
+RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=[mock,mock2]")
 echo $RES
 sleep 1000
-RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock,mock2")
+RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=[mock,mock2]")
 echo $RES
