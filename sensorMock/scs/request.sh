7c5ec26e91cba2e972835d8d068fee0ca3fa976c
enrikiko <enrikiko_91@hotmail.com>
2020-03-01 01:05:27 +0100

    Deep them draw many threat

--- sensorMock/scs/request.sh
+++ sensorMock/scs/request.sh
@@ -2,8 +2,8 @@ IP=$local_ip
 STATUS="false"
 SERVERIP="$IP:8000"
 echo "$SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock,mock2
-RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"devices=mock&devices=mock2&devices=mock3")
+RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock&devices=mock2&devices=mock3")
 echo $RES
 sleep 1000
-RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"devices=mock&devices=mock2&devices=mock3")
+RES=$(curl $SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock&devices=mock2&devices=mock3")
 echo $RES
