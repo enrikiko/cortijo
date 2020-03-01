57bfd9ff9f0aa9c5de8192fa617621b5a97e154a
enrikiko <enrikiko_91@hotmail.com>
2020-03-01 23:59:53 +0100

    Ability feel beyond finish there return passion

--- frontAngTest/src/app/config/config.component.ts
+++ frontAngTest/src/app/config/config.component.ts
@@ -50,7 +50,7 @@ export class ConfigComponent implements OnInit {
     const host = (window.location.href.split("/")[2]).split(":")[0]
     const url = "http://" + host + ":8000/config/update"
     let params = new HttpParams().set("config", configObject);
-    this.http.get(url, JSON.stringify({params: configObject})).subscribe( data =>
+    this.http.get(url, JSON.stringify({data: configObject})).subscribe( data =>
     {
       if(data!=null){
         console.log(data)
