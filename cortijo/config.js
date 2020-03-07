ba5f23e7a6f84cb2ea21003e174b8d7e564b9689
enrikiko <enrikiko_91@hotmail.com>
2020-03-07 18:44:00 +0100

    Couple arm could artist individual

--- cortijo/config.js
+++ cortijo/config.js
@@ -6,17 +6,22 @@ const config = yaml.safeLoad(config_file);
 //const TIMEOUT_SENSOR = config.timeout_sensor
 //const TIMEOUT_CHECK = config.timeout_check
 //
+function refresh_config() {
+  config_file = fs.readFileSync('config.yaml');
+  config = yaml.safeLoad(config_file);
+}
 module.exports = {
 
   getValues: () => {return config},
   get:  (value) => {return config[value]},
   safeValues: (obj) => {
-    console.log(obj);
-    console.log(toYaml.stringify(obj));
     fs.writeFile('./config.yaml', yaml.safeDump(toYaml.stringify(obj)), (err) => {
     if (err) {
         console.log(err);
       }
+    else {
+      refresh_config()
+    }
     });
   }
 
