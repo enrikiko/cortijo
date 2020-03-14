ed60a5977ac705ed8f8ccf823428ea2d1d8c80fa
enrikiko <enrikiko_91@hotmail.com>
2020-03-14 17:07:55 +0100

    Office point single reason

--- auth/url.js
+++ auth/url.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const yaml = require('js-yaml')
-var toYaml = require('json2yaml')
 let config_file = fs.readFileSync('config_url.yaml');
 let config = yaml.safeLoad(config_file);
 
