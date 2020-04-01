f1167d253776b4c959d15194986ef0893fdea357
enrikiko <enrikiko_91@hotmail.com>
2020-04-02 01:06:36 +0200

    Hang exist manage rock several authority class outside

--- feigenbaum/scs/init.py
+++ feigenbaum/scs/init.py
@@ -1,14 +1,14 @@
 from flask import Flask, escape, request
-growSensibility=int(0.01)
-periodeTimes=int(1000)
-humidity=int(1)
-grow=int(0.01)
+growSensibility = int(0.01)
+periodeTimes = int(1000)
+humidity = int(1)
+grow = int(0.01)
 app = Flask(__name__)
 
 def getHumidity():
-    for n in range(periodeTimes) :
-        humidity = grow * humidity * ( 1 - humidity )
-    grow += growSensibility
+    for n in range(global periodeTimes) :
+        global humidity = global grow * global humidity * ( 1 - global humidity )
+    global grow += global growSensibility
     return humidity
 
 
