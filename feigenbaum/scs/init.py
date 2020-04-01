85def0244ad817f4df0eea819fba580c95fd35f1
enrikiko <enrikiko_91@hotmail.com>
2020-04-02 01:20:11 +0200

    Section pattern who red view

--- feigenbaum/scs/init.py
+++ feigenbaum/scs/init.py
@@ -1,7 +1,7 @@
 from flask import Flask, escape, request, jsonify
 growSensibility = int(0.01)
 periodeTimes = int(1000)
-humidity = int(1)
+humidity = int(0.01)
 grow = int(0.01)
 app = Flask(__name__)
 
