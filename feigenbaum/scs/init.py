16818faf2fb00adc4f8e156b4ffebbdfa85e4373
enrikiko <enrikiko_91@hotmail.com>
2020-04-02 02:32:22 +0200

    Likely allow group

--- feigenbaum/scs/init.py
+++ feigenbaum/scs/init.py
@@ -1,18 +1,22 @@
 from flask import Flask, escape, request, jsonify
 growSensibility = float(0.005)
-periodeTimes = int(100)
+periodeTimes = int(1000)
 humidity = float(0.01)
 grow = float(1)
+times = 10
 app = Flask(__name__)
 
 def getHumidity():
     global periodeTimes
-    global humidity
+    humidity = float(0.1)
     global grow
     global growSensibility
+    global times
     for n in range(periodeTimes) :
         humidity = grow *  humidity * ( 1 - humidity )
-    grow += growSensibility
+    times -= 1
+    if time<=0:
+        grow += growSensibility
     return humidity * 1000
 
 
