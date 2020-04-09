a3074bfbb00b337e5664671bd763b9072b4c4e7f
enrikiko <enrikiko_91@hotmail.com>
2020-04-10 01:02:09 +0200

    Coach fund teach represent strong

--- feigenbaum/scs/request.sh
+++ feigenbaum/scs/request.sh
@@ -1,7 +1,7 @@
 IP=$local_ip
 SERVERIP="$IP:8000"
-echo "$SERVERIP"/newSensor/humidity/"$mock_name/$IP:$mock_port"
-RES=$(curl -X POST $SERVERIP"/sensor/humidity/"$mock_name/$IP:$mock_port)
+echo "$SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"
+RES=$(curl -X POST $SERVERIP"/sensor/temperature/"$mock_name/$IP:$mock_port)
 sleep 1000
-RES=$(curl -X POST $SERVERIP"/sensor/humidity/"$mock_name/$IP:$mock_port)
+RES=$(curl -X POST $SERVERIP"/sensor/temperature/"$mock_name/$IP:$mock_port)
 echo $RES
