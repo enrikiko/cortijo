e04ae9e63fbc0584d8627a7b6898e58b4dd9d40f
enrikiko <enrikiko_91@hotmail.com>
2020-04-11 01:35:51 +0200

    Finally serious section fund

--- frontAngTest/src/app/camera/camera.component.ts
+++ frontAngTest/src/app/camera/camera.component.ts
@@ -10,6 +10,9 @@ export class CameraComponent implements OnInit {
   url: string = "88.8.36.129";
   password: string = "tfuvjk765frvlnj";
   user : string = "admin";
+  autoPlay: string = "http://"+this.url+":8000/StrobeMediaPlayback.swf"
+  flashvars: string = "&amp;src=rtmp://"+this.url+":9981/flash/11:"+this.user+":"+this.password+"&amp;autoHideControlBar=true&amp;streamType=live&amp;autoPlay=true"
+
 
   constructor() { }
 
