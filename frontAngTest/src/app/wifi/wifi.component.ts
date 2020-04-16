b4b390fc2f5c89efa24c6ddf682c5033d6be85f0
enrikiko <enrikiko_91@hotmail.com>
2020-04-16 23:49:02 +0200

    Out two job what away them however

--- frontAngTest/src/app/wifi/wifi.component.ts
+++ frontAngTest/src/app/wifi/wifi.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as CanvasJS from './canvasjs.min';
 import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { SocketService } from '../socket.service';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-wifi',
