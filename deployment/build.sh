ba7f639867e60d88ada704458e69d6bc7bf65dc7
enrikiko <enrikiko_91@hotmail.com>
2020-04-21 16:16:57 +0200

    Cell story think product anyone him

--- deployment/build.sh
+++ deployment/build.sh
@@ -6,8 +6,8 @@ if [ -z "$2" ]
     version=$2
 fi
 pushd ~/Document/cortijo
-git checkout -- .
-git pull
+sudo git checkout -- .
+sudo git pull
 cd ~/Document/cortijo/deployment
 docker-compose build --build-arg MONGO_USER=user_name --build-arg MONGO_PASSWORD=user_password --build-arg PASSWORD=GordoCabron#Darwinex
 docker-compose up -d --remove-orphans
