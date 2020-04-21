f06f8a1aaa8084e6af29f9d675b7b703c750034e
enrikiko <enrikiko_91@hotmail.com>
2020-04-21 16:03:23 +0200

    Everything product common lose land it

--- deployment/build.sh
+++ deployment/build.sh
@@ -1,14 +1,14 @@
 #! bin/bash
 if [ -z "$2" ]
   then
-    version=$(python ~/cortijo/deployment/ramdonWord.py)
+    version=$(python ~/Document/cortijo/deployment/ramdonWord.py)
   else
     version=$2
 fi
-pushd ~/cortijo
+pushd ~/Document/cortijo
 git checkout -- .
 git pull
-cd ~/cortijo/deployment
+cd ~/Document/cortijo/deployment
 docker-compose build --build-arg MONGO_USER=user_name --build-arg MONGO_PASSWORD=user_password --build-arg PASSWORD=GordoCabron#Darwinex
 docker-compose up -d --remove-orphans
 if [ "$1" == "push" ]
