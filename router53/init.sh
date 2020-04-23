13a05bf100371e99767f2695e1ddbc9fb9146617
enrikiko <enrikiko_91@hotmail.com>
2020-04-24 01:37:35 +0200

    Bit make say good

--- router53/init.sh
+++ router53/init.sh
@@ -1,2 +1,13 @@
-curl $ROUTE53_URL --header "Content-Type: application/json" --data '{"password":"'${ROUTE53_PASSWORD}'"}' 2>/dev/null > /tmp/curl.out
-cat /tmp/curl.out
+#!/bin/bash
+while :
+do
+  curl -o /dev/null -s -w "%{http_code}\n" app.cortijodemazas.com > /tmp/curl.out
+  if [[ $(< /tmp/curl.out)  -eq 200 ]]; then
+    echo "Route53 is set correctly"
+  else
+    curl $ROUTE53_URL --header "Content-Type: application/json" --data '{"password":"'${ROUTE53_PASSWORD}'"}' 2>/dev/null > /tmp/curl.out
+    cat /tmp/curl.out
+    echo "Route53 have been set correctly"
+  fi
+	sleep 10
+done
