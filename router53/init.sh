58f11c85e2c92da2aaa3c48cee0c8c61d9b81fcc
enrikiko <enrikiko_91@hotmail.com>
2020-04-24 00:57:54 +0200

    Bill life sex black away save firm sign

--- router53/init.sh
+++ router53/init.sh
@@ -1,3 +1,3 @@
-curl $ROUTE53_URL --header "Content-Type: application/json" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' --silent > /tmp/curl.out
-echo $ROUTE53_URL --header "Content-Type: application/json" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' --silent
+curl $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' --silent > /tmp/curl.out
+echo $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' --silent
 cat /tmp/curl.out
