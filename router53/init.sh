333b96229593e0d847b0b8e46f8c0223f9745192
enrikiko <enrikiko_91@hotmail.com>
2020-04-24 01:11:51 +0200

    Join blue give street energy

--- router53/init.sh
+++ router53/init.sh
@@ -1,3 +1,3 @@
-curl $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' --silent > /tmp/curl.out
-echo $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' --silent
+curl $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' > /tmp/curl.out
+echo curl $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\'
 cat /tmp/curl.out
