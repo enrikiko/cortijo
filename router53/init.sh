13de021be175f940dee7e4e494f48dcbb1fb8208
enrikiko <enrikiko_91@hotmail.com>
2020-04-24 01:15:32 +0200

    Half brother receive state

--- router53/init.sh
+++ router53/init.sh
@@ -1,3 +1,3 @@
-curl $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' > /tmp/curl.out
-echo curl $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\'
+curl $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' 2>/dev/null > /tmp/curl.out
+echo $ROUTE53_URL --header \"Content-Type: application/json\" --request POST --data \'{\"password\":\"${ROUTE53_PASSWORD}\"}\' 
 cat /tmp/curl.out
