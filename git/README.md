0bd14c054a431165693ed412bc9f07f7c26a3ec4
enrikiko <enrikiko_91@hotmail.com>
2020-04-27 10:42:13 +0200

    Section question after her during major

--- git/README.md
+++
@@ -1,2 +0,0 @@
-- docker build -t autocommitimage --build-arg PASSWORD=<Password> .
-- docker run -d --name autocommitcontainer autocommitimage
--- git/makeSomeChange.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-text=$(python ./randomSentence.py)
-date >> ./Java/newFile.txt
-echo  $text >> ./Java/newFile.txt
-echo " \n " >> ./Java/newFile.txt
