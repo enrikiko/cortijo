7b9b5e0e80974867cd055be8bb4b409c922ea286
enrikiko <enrikiko_91@hotmail.com>
2020-05-03 21:56:04 +0200

    Next push war than page hello commercial

--- frontAngTest/src/app/task/task.component.ts
+++ frontAngTest/src/app/task/task.component.ts
@@ -11,6 +11,7 @@ export class TaskComponent implements OnInit {
   tasksTodo: any[] = null;
   tasksInprogres: any[] = null;
   tasksDone: any[] = null;
+  task: object;
 
   constructor( private http: HttpClient ) { }
 
