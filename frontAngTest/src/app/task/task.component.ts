f0df83ef4a525baa312dfe8ae2c993641eddeb7d
enrikiko <enrikiko_91@hotmail.com>
2020-05-03 22:17:54 +0200

    Rich article

--- frontAngTest/src/app/task/task.component.ts
+++ frontAngTest/src/app/task/task.component.ts
@@ -11,7 +11,7 @@ export class TaskComponent implements OnInit {
   tasksTodo: any[] = null;
   tasksInprogres: any[] = null;
   tasksDone: any[] = null;
-  task: {} = null;
+  task: object = {"name": null, "description": null};
 
   constructor( private http: HttpClient ) { }
 
