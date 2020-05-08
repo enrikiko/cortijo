e7e6446f53e76aa3dbece1187b73c32cbf70e8da
enrikiko <enrikiko_91@hotmail.com>
2020-05-08 14:30:36 +0200

    Beautiful treatment player

--- frontAngTest/src/app/humidity/humidity.component.ts
+++ frontAngTest/src/app/humidity/humidity.component.ts
@@ -48,8 +48,7 @@ subscription:any;
   }
 
   getData(sensor){
-      const host = (window.location.href.split("/")[2]).split(":")[0]
-      let url = "http://" + host + ":8000/all/" + sensor.type + "/" + sensor.name
+      let url = "http://back.app.cortijodemazas.com/all/" + sensor.type + "/" + sensor.name
       switch(sensor.type) {
         case "humidity":
           this.getHumidity(url, sensor.name)
