5605fb6fcc4d0cc1b9a882af59f234a90a640e6c
enrikiko <enrikiko_91@hotmail.com>
2020-05-09 18:23:38 +0200

    Clear history store point sometimes society daughter

--- s3/upload.py
+++ s3/upload.py
@@ -12,7 +12,7 @@ def upload_to_aws(local_file):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
                       aws_secret_access_key=SECRET_KEY)
     try:
-        s3.upload_file(local_file, BUCKETS_NAME)
+        s3.upload_file(local_file, BUCKETS_NAME, local_file)
         print("Upload Successful")
         return True
     except FileNotFoundError:
