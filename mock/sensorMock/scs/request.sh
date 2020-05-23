f236146a803b50ac87f5b79de8ff010146acb045
enrikiko <enrikiko_91@hotmail.com>
2020-05-23 22:59:54 +0200

    Likely good practice month

--- mock/sensorMock/scs/request.sh
+++ mock/sensorMock/scs/request.sh
@@ -1,9 +1,9 @@
 IP=$local_ip
-STATUS="false"
-SERVERIP="$IP:8000"
+STATUS="true"
+SERVERIP="back.app.cortijodemazas.com"
 echo "$SERVERIP"/newSensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock,mock2
 RES=$(curl -X POST $SERVERIP"/sensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock&devices=mock2&devices=mock3")
 echo $RES
-sleep 1000
-RES=$(curl -X POST $SERVERIP"/sensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock&devices=mock2&devices=mock3")
-echo $RES
+# sleep 1000
+# RES=$(curl -X POST $SERVERIP"/sensor/temperature/"$mock_name/$IP:$mock_port"?devices=mock&devices=mock2&devices=mock3")
+# echo $RES
