bfb4e9709b131ea9687c641716f4ad1ac92a8d01
enrikiko <enrikiko_91@hotmail.com>
2020-06-28 10:02:14 +0200

    Happy outside performance us mrs firm which eat

--- cortijo/joker.js
+++ cortijo/joker.js
@@ -94,7 +94,7 @@ module.exports={
          console.error(e);
        }finally{
          socket.deviceSocket(name+" has changed to "+status)
-         myDevicesChanges.newRequest(name, null, status, null)
+         myDevicesChanges.newRequest(name, null, status, "webSocket")
        }
        return response.statusCode;
      },
