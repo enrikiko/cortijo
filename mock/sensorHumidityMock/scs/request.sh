dfb807ba50281facdf8ec2cda408fc6077f8ad99
enrikiko <enrikiko_91@hotmail.com>
2020-07-19 17:30:08 +0200

    Without together business fish know middle process run

--- mock/sensorHumidityMock/scs/request.sh
+++ mock/sensorHumidityMock/scs/request.sh
@@ -1,8 +1,8 @@
 IP=$local_ip
 STATUS="true"
 SERVERIP="back.app.cortijodemazas.com"
-echo "$SERVERIP"/newSensor/humidity/"$mock_name/$IP:$mock_port"?devices=deviceMock,Device_1&min=300000&max=800000&lapse=1000
-RES=$(curl -X POST $SERVERIP"/sensor/humidity/"$mock_name/$IP:$mock_port"?devices=deviceMock,Device_1&min=300000&max=800000&lapse=1000")
+#echo "$SERVERIP"/newSensor/humidity/"$mock_name/$IP:$mock_port"?devices=deviceMock,Device_1&min=300000&max=800000&lapse=1000
+RES=$(curl -X POST $SERVERIP"/sensor/humidity/"$mock_name"/"$IP:$mock_port"?devices=Device_1,Device_3&min=300000&max=800000&lapse=1000")
 #sleep 1000
 #RES=$(curl -X POST $SERVERIP"/sensor/humidity/"$mock_name/$IP:$mock_port"?devices=deviceMock&min=200000&max=800000")
 echo $RES
