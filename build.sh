5d35914e7c7bd3b26c0a31c2ee28c816fb174976
enrikiko <enrikiko_91@hotmail.com>
2020-10-25 20:06:51 +0100

    Paper technology mrs

---
+++ build.sh
@@ -0,0 +1,12 @@
+sudo git pull \
+docker-compose up -d --build \
+--build-arg API_URL=$API_URL \
+--build-arg API_KEY=$API_KEY \
+--build-arg PASSWORD=$GIT_PASSWORD \
+--build-arg ACCESS_KEY=$ACCESS_KEY \
+--build-arg SECRET_KEY=$SECRET_KEY \
+--build-arg ROUTE53_URL=$ROUTE53_URL \
+--build-arg BUCKETS_NAME=$BUCKETS_NAME \
+--build-arg ROUTE53_PASSWORD=$ROUTE53_PASSWORD \
+&& \
+docker-compose up -d
