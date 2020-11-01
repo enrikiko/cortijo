007c4382578f43d0709a88e71add95f11719299e
Enrique Ramos <enrikiko_91@hotmail.com>
2020-11-01 05:38:22 +0100

    Race smile once including environment set cup

--- mock/deviceMock/scs/request.sh
+++ mock/deviceMock/scs/request.sh
@@ -3,7 +3,7 @@ STATUS="false"
 SERVERIP="back.app.cortijodemazas.com"
 TENANT="cortijo"
 echo "$SERVERIP"/new/"$TENANT/$mock_name/$STATUS/$IP:$mock_port"
-RES=$(curl -X POST $SERVERIP"/device/"$mock_name/$STATUS/$IP:$mock_port)
+RES=$(curl -X POST $SERVERIP"/device/"$TENANT/$mock_name/$STATUS/$IP:$mock_port)
 # sleep 1000
 # RES=$(curl -X POST $SERVERIP"/device/"$mock_name/$STATUS/$IP:$mock_port)
 echo $RES
