08860ad0483a48ef059789c96e138c026fd38864
Enrique Ramos <enrikiko_91@hotmail.com>
2020-11-08 00:09:27 +0100

    Victim economy it little which nature

--- ws/init.js
+++ ws/init.js
@@ -92,13 +92,12 @@ function getMsg(message) {
 
 async function getDeviceData(tenant, device){
   let result
-  await Promise.all(wss.clients.forEach( function(client) {
+  await Promise.all(wss.clients.map( function(client) {
    if (client.name == device &&  client.tenant == tenant) { //client.isAlive == true &&
        client.send("data")
        logs(['Asking for data...']);
        client.on('message', function(message) {
          message = JSON.parse(message)
-         console.log(typeof message);
          result=message
          if (message.device==device) {
            console.log("Eureka");
