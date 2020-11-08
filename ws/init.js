8608d5a8b51d5724b00a5e9e4a9f92231ed97700
Enrique Ramos <enrikiko_91@hotmail.com>
2020-11-09 00:51:15 +0100

    Suddenly democratic buy some line business green

--- ws/init.js
+++ ws/init.js
@@ -184,8 +184,10 @@ async function updateDevice(tenant, device, id, status) {
   //
   //   }
   // })
+  console.log(id);
   await deviceStatus.updateDevice(tenant, device, status)
-  device_map.id.send(status)
+  console.log(device_map[id])
+  device_map[id].send(status)
   if (certain) {
     return true
   }
