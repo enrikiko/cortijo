0d75a35e91a4481c62c90dcf0ec13602b6e1444c
Enrique Ramos <enrikiko_91@hotmail.com>
2020-11-17 19:07:55 +0100

    Beyond wear black form play disease

--- count/init.js
+++ count/init.js
@@ -4,7 +4,7 @@ let count = 0
 app.enable('trust proxy');
 app.get('/count', function (req, res) {
   res.status(200).json({"index": ++count})
-  console.log({"index": count});
+  console.log({"index": count, "time": new Date().toLocaleString({timeZone: 'Europe/Spain'})});
 })
 
 app.listen(3000)
