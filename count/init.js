b31997682cd3df2201285bb59838c94b6fcd0b25
Enrique Ramos <enrikiko_91@hotmail.com>
2020-11-18 20:29:35 +0100

    Fish begin paper spend smile figure

--- count/init.js
+++ count/init.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
 let count = 38091640
+let lastcount = count
+let speed = 0
 app.enable('trust proxy');
 app.get('/count', function (req, res) {
   res.status(200).json({"index": --count})
-  console.log({"index": count, "time": new Date().toLocaleString({timeZone: 'Europe/Spain'})});
+  console.log({"index": count, "time": new Date().toLocaleString({timeZone: 'Europe/Spain'}), "speed": speed});
 })
+
+function startTime(){
+    setTimeout(function(){
+            speed = lastcount-count
+            lastcount = count
+            startTime()
+        }, 1000);
+}
 
 app.listen(3000)
