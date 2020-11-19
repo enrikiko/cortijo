d024127cca6eac3d3b834bf1f7fe3bfbb9ac4229
enrikiko <enrikiko_91@hotmail.com>
2020-11-19 21:39:24 +0100

    2

---
+++ count2/init.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+let count = 38091640
+let lastcount = count
+let speed = 0
+app.enable('trust proxy');
+app.get('/count', function (req, res) {
+  res.status(200).json({"index": --count})
+  console.log({"index": count, "time": new Date().toLocaleString({timeZone: 'Europe/Spain'}), "speed": speed+"ps"});
+})
+
+function startTime(){
+    setTimeout(function(){
+            speed = lastcount - count
+            lastcount = count
+            startTime()
+        }, 1000);
+}
+startTime()
+app.listen(3000)
