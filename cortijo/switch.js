7f75f506e1820500c869638e45787924a46f5a85
Enrique Ramos <enrikiko_91@hotmail.com>
2020-11-22 14:47:14 +0100

    Serious else keep state

--- cortijo/switch.js
+++ cortijo/switch.js
@@ -5,8 +5,10 @@ const myDevicesChanges = require('./devicesChanges');
 
 let timeOutMap={}
 
-async function changeBackFalse(tenant, name) {
+async function changeBackFalse(device) {
   //Change back to false
+  tenant = device.tenant
+  name = device.name
   try {
       var responseBack = await request.switchStatus(tenant, false, name) //Change device status
       if (responseBack.code == 200) {
@@ -69,7 +71,7 @@ module.exports = {
               var response = await request.switchStatus(tenant, true, name) //Change device status
               if (response.code == 200) {
                 await myDevicesChanges.newRequest(tenant, name, true, user, lapse)
-                timeOutMap[tenant][name] = setTimeout(changeBackFalse, lapse, (tenant, name));
+                timeOutMap[tenant][name] = setTimeout(changeBackFalse, lapse, {"tenant":tenant, "name":name});
                 request.telegramAlert(name, lapse, user);
                 if(res!=null){  //TODO is this nessesary?
                   res.status(response.code).send(response)
