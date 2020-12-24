3da8a141bb747614b8263289e0416b06b4dc806e
Enrique Ramos <enrikiko_91@hotmail.com>
2020-12-24 18:14:51 +0100

    That inside us study as could

--- router53/init.sh
+++ router53/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while :
 do
-  curl -o /dev/null -s -w "%{http_code}\n" app.cortijodemazas.com > /tmp/curl.out
-  if [[ "$(cat /tmp/curl.out)" == "200" ]]; then
+  curl -o /dev/null -s -w "%{http_code}\n" back.app.cortijodemazas.com > /tmp/curl.out
+  if [[ "$(cat /tmp/curl.out)" == "401" ]]; then
     date
     echo "Route53 is set correctly"
   else
