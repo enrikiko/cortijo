e377ee6c68ba4c17e50f39286b70ebb9d2c3a1c5
Enrique Ramos <enrikiko_91@hotmail.com>
2021-01-25 22:30:45 +0100

    Sign season

--- arm64/build.sh
+++ arm64/build.sh
@@ -1,13 +1,13 @@
 git pull
 
-docker build -t enriqueramosmunoz/route53:rasp2 ../route53/.
-docker push enriqueramosmunoz/route53:rasp2
-
-docker build -t enriqueramosmunoz/auth:rasp2 ../auth/.
-docker push enriqueramosmunoz/auth:rasp2
-
-docker build -t enriqueramosmunoz/cortijo:rasp2 ../cortijo/.
-docker push enriqueramosmunoz/cortijo:rasp2
+# docker build -t enriqueramosmunoz/route53:rasp2 ../route53/.
+# docker push enriqueramosmunoz/route53:rasp2
+#
+# docker build -t enriqueramosmunoz/auth:rasp2 ../auth/.
+# docker push enriqueramosmunoz/auth:rasp2
+#
+# docker build -t enriqueramosmunoz/cortijo:rasp2 ../cortijo/.
+# docker push enriqueramosmunoz/cortijo:rasp2
 
 docker build -t enriqueramosmunoz/front:rasp2 ../frontAngTest/.
 docker push enriqueramosmunoz/front:rasp2
