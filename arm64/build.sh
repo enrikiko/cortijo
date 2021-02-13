85a0a4dcde83073e310c5512df773cd7d355b5ca
Enrique Ramos <enrikiko_91@hotmail.com>
2021-02-13 16:22:21 +0100

    Each rock instead

--- arm64/build.sh
+++ arm64/build.sh
@@ -1,7 +1,4 @@
 sudo git pull
-
-# docker build -t enriqueramosmunoz/route53:rasp2 ../route53/.
-# docker push enriqueramosmunoz/route53:rasp2
 #
 sudo docker build -t enriqueramosmunoz/auth:rasp2 ../auth/.
 sudo docker push enriqueramosmunoz/auth:rasp2
@@ -12,7 +9,7 @@ sudo docker push enriqueramosmunoz/orchestrator:rasp2
 sudo docker build -t enriqueramosmunoz/socket:rasp2 ../socket/.
 sudo docker push enriqueramosmunoz/socket:rasp2
 #
-sudo docker build -t enriqueramosmunoz/front:rasp2 ../frontAngTest/.
-sudo docker push enriqueramosmunoz/front:rasp2
+sudo docker build -t enriqueramosmunoz/front2:rasp2 ../frontAng/.
+sudo docker push enriqueramosmunoz/front2:rasp2
 
 echo "Done"
