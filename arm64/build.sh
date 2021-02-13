675c410f7c0e19ed8591e407083932f7b7e67ac5
Enrique Ramos <enrikiko_91@hotmail.com>
2021-02-13 17:07:47 +0100

    Reason identify if international detail once this

--- arm64/build.sh
+++ arm64/build.sh
@@ -1,15 +1,18 @@
 sudo git pull
 #
-sudo docker build -t enriqueramosmunoz/auth:rasp2 ../auth/.
-sudo docker push enriqueramosmunoz/auth:rasp2
+sudo docker build -t enriqueramosmunoz/auth:rasp ../auth/.
+sudo docker push enriqueramosmunoz/auth:rasp
 #
-sudo docker build -t enriqueramosmunoz/orchestrator:rasp2 ../orchestrator/.
-sudo docker push enriqueramosmunoz/orchestrator:rasp2
+sudo docker build -t enriqueramosmunoz/orchestrator:rasp ../orchestrator/.
+sudo docker push enriqueramosmunoz/orchestrator:rasp
 #
-sudo docker build -t enriqueramosmunoz/socket:rasp2 ../socket/.
-sudo docker push enriqueramosmunoz/socket:rasp2
+sudo docker build -t enriqueramosmunoz/socket:rasp ../socket/.
+sudo docker push enriqueramosmunoz/socket:rasp
 #
-sudo docker build -t enriqueramosmunoz/front2:rasp2 ../frontAng/.
-sudo docker push enriqueramosmunoz/front2:rasp2
+sudo docker build -t enriqueramosmunoz/front2:rasp ../frontAng/.
+sudo docker push enriqueramosmunoz/front2:rasp
+#
+sudo docker build -t enriqueramosmunoz/setip:rasp ../setip/.
+sudo docker push enriqueramosmunoz/setip:rasp
 
 echo "Done"
